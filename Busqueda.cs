f90c4cc918f07e58ab17452fc724e7d0aa76ec43
Rotei <roberto.sanchez@campusfp.es>
2020-04-01 11:15:37 +0200

    Búsqueda parcial y default de menú

---
+++ Busqueda.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parte_Git
+{
+    /*Búsqueda parcial a partir de una letra o subconjunto de letras (en el caso de encontrar ese fichero deberá mostrar todos los datos que le pertenecen*/
+    class Busqueda
+    {
+        //int contador = 0; (fuera del menú)
+
+        /*string letra = Console.ReadLine();
+        Console.WriteLine("Escribe una letra : ");
+            string buscar = Console.ReadLine();
+        do
+        {
+        Console.WriteLine("Lo siento, no has escrito nada");
+            Console.WriteLine("Vuelve a introducir una letra");
+            buscar = Console.ReadLine();
+        }while (buscar == "");
+
+        Console.WriteLine("Ahora le mostraré los datos revelados con su búsqueda: {0} ", buscar);
+        for (int i = 0; i<contador; i++)
+			{
+                if(     .ToLower().Contains(looking.ToLower()))
+                   {
+
+                   }
+			}
+          break;*/
+
+
+
+
+
+
+      /*default: 
+        {
+        Console.WriteLine(" \n No te entiendo, has dado un valor que no coincide con el menú ");
+            break;
+        }*/
+
+    }
+}
