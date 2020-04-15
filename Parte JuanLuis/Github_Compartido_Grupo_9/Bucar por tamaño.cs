d8ed35a9f4a849eff4857d6e60a4988df41ea5b0
Xora98 <62642393+Xora1998@users.noreply.github.com>
2020-04-15 09:36:20 +0200

    Add files via upload

---
+++ Parte JuanLuis/Github_Compartido_Grupo_9/Bucar por tamaño.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Github_Compartido_Grupo_9
+{
+    class Bucar_por_tamaño
+    {
+        Console.WriteLine("\nDime un tamaño que quieres que busque y te mostrare los datos del juego con ese tamaño");
+            string palabra = Console.ReadLine();
+            for (int i = 0; i<contador; i++)
+            {
+                if (palabra == arrjuegos[i].juego.tamaño)
+                {
+                    Console.WriteLine("\nTitulo: {0} \nGenero: {1} \nTamaño: {2} \nFecha: {3} KB \nDesarrolladores: {4} \nFranquicia: {5} \nNacionalidad: {6}",
+                        arrjuegos[i].juego.titulo, arrjuegos[i].juego.genero, arrjuegos[i].juego.tamaño, arrjuegos[i].juego.fecha, arrdesarrolladores[i].desarrolladores, arrfranquicia[i].franquicia, arrnacionalidad[i].nacionalidad);
+                }
+}
+    }
+}
